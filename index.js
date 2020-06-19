a60468da1d2f4c6e4ea2a0e68af5d5d7e3ffef3e
Rafael Menon <rafael13rodrigo@gmail.com>
2020-06-19 13:36:21 -0300

    port express

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(`${baseDir}`));
 
 app.get("*", (req, res) => res.sendFile("index.html", { root: baseDir }));
 
-const port = 80;
+const port = 81;
 
 app.listen(port, () =>
   console.log(`Servidor subiu com sucesso em http://localhost:${port}`)
